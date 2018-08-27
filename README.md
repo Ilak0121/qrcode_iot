d9570a1fe25fe71c3cc33ecd7f87d3be47dd6f66
ilak0121 <s7515199@gmail.com>
2018-08-28 01:27:35 +0900

    backup

---
+++ README.md
@@ -0,0 +1,8 @@
+QR code image processing project
+
+QR code has 3 information about the code.
+1. type
+2. data
+3. location
+
+
--- test.py
+++ test.py
@@ -1,17 +1,36 @@
-from pyzbar.pyzbar import decode
-from PIL import Image
+#from PIL import Image
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+import cv2
 
-def constructor():
-    print("\n\n")
-    print("------------------------starting---------------------------")
+def decode(im):
+    #find barcodes and QRcode
+    decodedObjects=pyzbar.decode(im)
 
-def destructor():
-    print("------------------------ending---------------------------")
-    print("\n\n")
+    #print results
+    for obj in decodedObjects:
+        print('Type : ',obj.type)
+        print('Data : ',obj.data,'\n')
+    return decodedObjects
+
+#display barcode and QR code location
+def display(im, decodedObjects):
+    for decodedObject in decodedObjects:
+        points=decodedObject.polygon
+
+        if len(points) >4:
+            hull=cv2.convexHull(np.array([point for point in points],dtype=np.float32))
+            hull=list(map(tuple,np.squeeze(hull)))
+        else:
+            hull=points
+
+        n = len(hull)
+
+        for j in range(0,n):
+            cv2.line(im,hull[j],hull[(j+1)%n],(255,0,0),3)
+    sv2.imshow(
 
 constructor()
-a=decode(Image.open('./test.png'))
-print("\n\n")
+a=decode(cv2.imread('./code.png'))
 print(a)
-
 destructor()
